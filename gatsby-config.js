7e8d28d5d802f98a60a9bad5692a41f5502e4938
OtacilioN <otacilio.n97@gmail.com>
2020-03-08 11:19:20 -0300

    Modificando os arquivos da estrutura

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Café com Ota`,
+    description: `Este blog fala sobre cafés, a vida, o universo e tudo mais, escrito por Otacilio Maia.`,
+    author: `@OtacilioMaia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#6f4e37`,
+        theme_color: `#6f4e37`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Ops não encontrei</h1>
+    <p>Tem certeza que deveria ter algo aqui?</p>
   </Layout>
 )
 
